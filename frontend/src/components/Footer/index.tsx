b91fdce7666f21b3eb8cde02e283f927ce23f9ba
Gabriel Manteigas <40912@alunos.isel.ipl.pt>
2021-11-06 14:23:03 +0000

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,34 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">
+          App desenvolvida por{" "}
+          <a
+            href="https://github.com/GabrielManteigas"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Gabriel Manteigas
+          </a>
+        </p>
+        <p className="text-light">
+          <small>
+            <strong>Semana Spring React</strong>
+            <br />
+            Evento promovido pela escola DevSuperior:{" "}
+            <a
+              href="https://instagram.com/devsuperior.ig"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @devsuperior.ig
+            </a>
+          </small>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
