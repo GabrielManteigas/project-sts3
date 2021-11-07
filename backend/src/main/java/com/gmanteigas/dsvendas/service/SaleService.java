df8adb45d777c749ecece65103ef930601e46ccf
Gabriel Manteigas <40912@alunos.isel.ipl.pt>
2021-11-07 04:56:34 +0000

    Group by search

--- backend/src/main/java/com/gmanteigas/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/gmanteigas/dsvendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.gmanteigas.dsvendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.gmanteigas.dsvendas.dto.SaleDTO;
+import com.gmanteigas.dsvendas.dto.SaleSuccessDTO;
+import com.gmanteigas.dsvendas.dto.SaleSumDTO;
 import com.gmanteigas.dsvendas.entities.Sale;
 import com.gmanteigas.dsvendas.repositories.SaleRepository;
 import com.gmanteigas.dsvendas.repositories.SellerRepository;
@@ -28,5 +32,14 @@ public class SaleService {
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(s-> new SaleDTO(s));
 	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
 
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
+	}
 }
